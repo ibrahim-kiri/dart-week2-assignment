9294268e9069d2850b38e5c8f4e4e65cefcfd561
ibrahim-kiri <ibrahimkiri@gmail.com>
2024-03-09 08:36:03 +0300

    week assignment

---
+++ data_types.dart
@@ -0,0 +1,27 @@
+void main() {
+  // Interger data type (int)
+  int age = 30;
+
+  // Double data type (double)
+  double height = 5.8;
+
+  // String data type (String)
+  String name = 'Kiri Ibrahim';
+
+  // List data type (List)
+  List<int> numbers = [1, 2, 3, 4, 5];
+
+  // Map data type (Map)
+  Map<String, dynamic> person = {
+    'name': 'Lyn Kat',
+    'age': 25,
+    'isStudent': true,
+  };
+
+  // Output
+  print('Age: $age');
+  print('Height: $height');
+  print('Name: $name');
+  print('Numbers: $numbers');
+  print('Person: $person');
+}
